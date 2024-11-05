701e7e2bc0b321229db254c21d6b7313b1a766ec
Vishnukannamkumarath <165531187+Vishnukannamkumarath@users.noreply.github.com>
2024-11-05 08:50:53 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import './App.css';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Home from './components/signup';
-import Lerror from './components/error';
-import Match from './components/notmatch';
-import HomeP from './components/homepage'
-import Quiz from './components/questions'
+import Home from './component/Register'
+import Run from './component/Login'
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/signup' element={<Home/>}></Route>
-      <Route path='/error' element={<Lerror/>}></Route>
-      <Route path='/notmatch' element={<Match/>}></Route>
-      <Route path='/homepage' element={<HomeP/>}></Route>
-      <Route path='/questions' element={<Quiz/>}></Route>
+    <Routes >
+      <Route path='/Login' element={<Run/>}></Route> 
+      <Route path="/Register" element={<Home/>} ></Route> 
     </Routes>
+    
+    
+    
     </BrowserRouter>
+  
   );
 }
 
